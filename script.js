b6cb152ffad58c45f79f2ffa7e469f13bbc6a1cd
Shin <luffy-karugara@hotmail.fr>
2022-10-03 18:06:35 +0200

    Add functionalities and styles/visuals

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let operand1 = 0;
 let operand2 = 0;
 let operator = '';
 let result = 0;
+let lengthCounter = 0;
 
 const add = function (a, b) {
     return a + b;
@@ -19,18 +20,25 @@ const divide = function (a, b) {
     return a / b;
 }
 
+const modulo = function (a, b) {
+    return a % b;
+}
+
 const operate = function (operator, a, b) {
     if (operator === '+') {
         return add(a, b);
     }
-    if (operator === '−') {
+    if (operator === '−' || operator === '-') {
         return substract(a, b);
     }
-    if (operator === '×') {
+    if (operator === '×' || operator === '*') {
         return multiply(a, b);
     }
-    if (operator === '÷') {
+    if (operator === '÷' || operator === '/') {
         return divide(a, b);
+    }
+    if (operator === '%') {
+        return modulo(a, b);
     }
 }
 
@@ -43,10 +51,12 @@ buttons.forEach((button) => {
             if (operand1 === 0) {
                 operand1 = Number(button.textContent);
                 display.textContent = operand1;
-                console.log(typeof operand1);
             }
-            else if (operand1 !== 0 && operator === '') {
-                console.log(typeof operand1);
+            else if (operand1 !== 0 && operator === '' && ((operand1.toString()).includes('.'))) {
+                operand1 = operand1 + Number(button.textContent);
+                display.textContent = operand1;
+            }
+            else if (operand1 !== 0 && operator === '' && !((operand1.toString()).includes('.'))) {
                 operand1 = operand1 * 10 + Number(button.textContent);
                 display.textContent = operand1;
             }
@@ -55,16 +65,24 @@ buttons.forEach((button) => {
                     operand2 = Number(button.textContent);
                     display.textContent = `${operand1} ${operator} ${operand2}`;
                 }
-                else if (operand2 !== 0) {
+                else if (operand2 !== 0 && !((operand2.toString()).includes('.'))) {
                     operand2 = operand2 * 10 + Number(button.textContent);
+                    display.textContent = `${operand1} ${operator} ${operand2}`;
+                }
+                else if (operand2 !== 0 && ((operand2.toString()).includes('.'))) {
+                    operand2 = operand2 + Number(button.textContent);
                     display.textContent = `${operand1} ${operator} ${operand2}`;
                 }
             }
         }
-        else if (button.textContent === '+' || button.textContent === '−' || button.textContent === '×' || button.textContent === '÷') {
+        else if (button.textContent === '+' || button.textContent === '−' || button.textContent === '×' || button.textContent === '÷' || button.textContent === '%') {
             if (operand1 !== 0 && operand2 === 0 && operator === '') {
                 operator = button.textContent;
                 display.textContent = `${operand1} ${button.textContent}`;
+            }
+            else if (operand1 !== 0 && operand2 === 0 && operator !== '') {
+                operator = button.textContent;
+                display.textContent = `${operand1} ${operator}`;
             }
             else if (result !== 0) {
                 operand1 = result;
@@ -74,15 +92,14 @@ buttons.forEach((button) => {
             }
             else if (operand1 !== 0 && operator !== '') {
                 if (operator === '÷' && operand2 === 0) {
-                    console.log(operand2);
                     display.textContent = 'Cannot divide by zero';
                     operand1 = 0;
                     operand2 = 0;
                     operator = '';
                 }
                 else {
-                    operand1 = operate(operator, operand1, operand2);
-                    console.log(operand1);
+                    // operand1 = (operate(operator, Number(operand1), Number(operand2))).toFixed(7);
+                    operand1 = operate(operator, Number(operand1), Number(operand2));
                     operand2 = 0;
                     operator = button.textContent;
                     display.textContent = `${operand1} ${operator}`;
@@ -93,20 +110,30 @@ buttons.forEach((button) => {
             operand1 = 0;
             operand2 = 0;
             operator = '';
+            result = 0;
             display.textContent = 0;
         }
         else if (button.textContent === '⌫') {
-            if (operand2 === 0 && operand1 !== 0 && operator === '') {
+            if (operand2 === 0 && operand1 !== 0 && operator === '' && !((operand1.toString()).includes('.'))) {
                 operand1 = Math.floor(operand1 / 10);
                 display.textContent = operand1;
             }
-            if (operand1 !== 0 && operand2 !== 0) {
+            else if (operand1 !== 0 && operand2 !== 0 && !((operand2.toString()).includes('.'))) {
                 operand2 = Math.floor(operand2 / 10);
+                display.textContent = `${operand1} ${operator} ${operand2}`;
+            }
+            else if (operand1 !== 0 && operator === '' && ((operand1.toString()).includes('.'))) {
+                operand1 = operand1.slice(0, operand1.length - 1);
+                display.textContent = operand1;
+            }
+            else if (operand1 !== 0 && operator !== '' && operand2 !== 0 && ((operand2.toString()).includes('.'))) {
+                operand2 = operand2.slice(0, operand2.length - 1);
                 display.textContent = `${operand1} ${operator} ${operand2}`;
             }
         }
         else if (button.textContent === '=' && operand1 !== 0 && operand2 !== 0) {
-            result = operate(operator, operand1, operand2);
+            // result = (operate(operator, Number(operand1), Number(operand2))).toFixed(12);
+            result = operate(operator, Number(operand1), Number(operand2));
             display.textContent = result;
             operand1 = 0;
             operand2 = 0;
@@ -119,5 +146,147 @@ buttons.forEach((button) => {
             operand2 = 0;
             operator = '';
         }
+        else if (button.textContent === '.') {
+            if (operand1 === 0) {
+                operand1 = operand1 + button.textContent;
+                display.textContent = operand1;
+            }
+            else if (operand1 !== 0 && operator === '' && !((operand1.toString()).includes('.'))) {
+                operand1 = operand1 + button.textContent;
+                display.textContent = operand1;
+            }
+            else if (operand1 !== 0 && operator !== '' && operand2 === 0) {
+                operand2 = operand2 + button.textContent;
+                display.textContent = `${operand1} ${operator} ${operand2}`;
+            }
+            else if (operand1 !== 0 && operator !== '' && operand2 !== 0 && !((operand2.toString()).includes('.'))) {
+                operand2 = operand2 + button.textContent;
+                display.textContent = `${operand1} ${operator} ${operand2}`;
+            }
+        }
     });
+});
+
+
+
+
+document.addEventListener('keydown', function (e) {
+    if (e.code === 'Numpad0' || e.code === 'Numpad1' || e.code === 'Numpad2' || e.code === 'Numpad3' || e.code === 'Numpad4' || e.code === 'Numpad5' || e.code === 'Numpad6' || e.code === 'Numpad7' || e.code === 'Numpad8' || e.code === 'Numpad9') {
+        if (operand1 === 0) {
+            operand1 = Number(e.key);
+            display.textContent = operand1;
+        }
+        else if (operand1 !== 0 && operator === '' && !(operand1.toString()).includes('.')) {
+            operand1 = operand1 * 10 + Number(e.key);
+            display.textContent = operand1;
+        }
+        else if (operand1 !== 0 && operator === '' && ((operand1.toString()).includes('.'))) {
+            operand1 = operand1 + Number(e.key);
+            display.textContent = operand1;
+        }
+        else if (operand1 !== 0 && operator !== '') {
+            if (operand2 === 0) {
+                operand2 = Number(e.key);
+                display.textContent = `${operand1} ${operator} ${operand2}`;
+            }
+            else if (operand2 !== 0 && !((operand2.toString()).includes('.'))) {
+                operand2 = operand2 * 10 + Number(e.key);
+                display.textContent = `${operand1} ${operator} ${operand2}`;
+            }
+            else if (operand2 !== 0 && ((operand2.toString()).includes('.'))) {
+                operand2 = operand2 + Number(e.key);
+                display.textContent = `${operand1} ${operator} ${operand2}`;
+            }
+        }
+    }
+
+    else if (e.code === 'NumpadAdd' || e.code === 'NumpadSubtract' || e.code === 'NumpadMultiply' || e.code === 'NumpadDivide') {
+        if (operand1 !== 0 && operand2 === 0 && operator === '') {
+            operator = e.key;
+            display.textContent = `${operand1} ${e.key}`;
+        } 
+        else if (operand1 !== 0 && operand2 === 0 && operator !== '') {
+            operator = e.key;
+            display.textContent = `${operand1} ${operator}`;
+        }
+        else if (result !== 0) {
+            operand1 = result;
+            operator = e.key;
+            result = 0;
+            display.textContent = `${operand1} ${e.key}`;
+        }
+        else if (operand1 !== 0 && operator !== '') {
+            if (operator === '÷' && operand2 === 0) {
+                display.textContent = 'Cannot divide by zero';
+                operand1 = 0;
+                operand2 = 0;
+                operator = '';
+            }
+            else if (operand1 !== 0 && operand2 !== 0 && operator !== '') {
+                // operand1 = (operate(operator, Number(operand1), Number(operand2))).toFixed(7);
+                operand1 = operate(operator, Number(operand1), Number(operand2));
+                operand2 = 0;
+                operator = e.key;
+                display.textContent = `${operand1} ${operator}`;
+            }
+        }
+    }
+    else if (e.code === 'Escape') {
+        operand1 = 0;
+        operand2 = 0;
+        result = 0;
+        operator = '';
+        display.textContent = 0;
+    }
+    else if (e.code === 'Backspace') {
+        if (operand2 === 0 && operand1 !== 0 && operator === '' && !((operand1.toString()).includes('.'))) {
+            operand1 = Math.floor(operand1 / 10);
+            display.textContent = operand1;
+        }
+        else if (operand2 === 0 && operand1 !== 0 && operator === '' && ((operand1.toString()).includes('.'))) {
+            operand1 = operand1.slice(0, operand1.length - 1);
+            display.textContent = operand1;
+        }
+        else if (operand1 !== 0 && operand2 !== 0 && !((operand2.toString()).includes('.'))) {
+            operand2 = Math.floor(operand2 / 10);
+            display.textContent = `${operand1} ${operator} ${operand2}`;
+        }
+        else if (operand1 !== 0 && operand2 !== 0 && ((operand2.toString()).includes('.'))) {
+            operand2 = operand2.slice(0, operand2.length - 1);
+            display.textContent = `${operand1} ${operator} ${operand2}`;
+        }
+    }
+    else if (operand1 !== 0 && operand2 !== 0 && (e.code === 'NumpadEnter' || e.code === 'Enter')) {
+        // result = (operate(operator, Number(operand1), Number(operand2))).toFixed(11);
+        result = operate(operator, Number(operand1), Number(operand2));
+        display.textContent = result;
+        operand1 = 0;
+        operand2 = 0;
+        operator = '';
+    }
+    else if (operand1 !== 0 && operand2 === 0 && operator === '÷' && (e.code === 'NumpadEnter' || e.code === 'Enter')) {
+        // result = operate(operator, operand1, operand2);
+        display.textContent = 'Cannot divide by zero';
+        operand1 = 0;
+        operand2 = 0;
+        operator = '';
+    }
+    else if (e.code === 'NumpadDecimal') {
+        if (operand1 === 0) {
+            operand1 = operand1 + e.key;
+            display.textContent = operand1;
+        }
+        else if (operand1 !== 0 && operator === '' && !((operand1.toString()).includes('.'))) {
+            operand1 = operand1 + e.key;
+            display.textContent = operand1;
+        }
+        else if (operand1 !== 0 && operator !== '' && operand2 === 0) {
+            operand2 = operand2 + e.key;
+            display.textContent = `${operand1} ${operator} ${operand2}`;
+        }
+        else if (operand1 !== 0 && operator !== '' && operand2 !== 0 && !((operand2.toString()).includes('.'))) {
+            operand2 = operand2 + e.key;
+            display.textContent = `${operand1} ${operator} ${operand2}`;
+        }
+    }
 });
